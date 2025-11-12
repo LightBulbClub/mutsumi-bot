9029f05e5609a92a67a4ea32c31ff94ed2553d7b
cloudw233 <qrz20081004@outlook.com>
2025-11-12 23:10:51 +0800

    u

--- modules/husband/__init__.py
+++ modules/husband/__init__.py
@@ -2,20 +2,19 @@ from core.builtins.bot import Bot
 from core.builtins.message.internal import I18NContext, Image, Plain
 from core.component import module
 from core.constants.path import assets_path
-from .database.models import TodayWifeInfo
-
+from .database.models import TodayHusbandInfo
 from datetime import datetime
 import os
 import random
 
-wif = module(
-    "wife",
-    {"waifu": "wife", "jrlp": "wife", "hlp": "wife change"},
-    "获取今日二次元老婆",
+hsb = module(
+    "husband",
+    {"jrlg": "husband", "hlg": "husband change"},
+    "获取今日二次元老公",
     developers=["haoye_qwq"],
 )
 
-assets = assets_path / "modules" / "wife"
+assets = assets_path / "modules" / "husband"
 
 wife_names = os.listdir(assets)
 
@@ -24,7 +23,7 @@ async def waifu(msg: Bot.MessageSession, change_: bool):
     change = change_
     _id = msg.session_info.sender_id
     chose = random.sample(wife_names, 1)[0]
-    db = await TodayWifeInfo.get_or_none(sender_id=_id)
+    db = await TodayHusbandInfo.get_or_none(sender_id=_id)
     wife_now = None
     if db:
         if db.timestamp.date != datetime.date:
@@ -32,17 +31,17 @@ async def waifu(msg: Bot.MessageSession, change_: bool):
         else:
             wife_now = db.wife_name
     if change or not db:
-        if await TodayWifeInfo.get_wife(sender_id=_id, name=chose):
+        if await TodayHusbandInfo.get_wife(sender_id=_id, name=chose):
             await msg.finish(
                 [
-                    Plain("成功！你今天的老婆是"),
+                    Plain("成功！你今天的老公是"),
                     Plain(chose.split(".")[0]),
                     Image(assets / chose),
                 ]
             )
         await msg.finish(
             [
-                Plain("失败！你今天换老婆次数太多了，你今天的老婆只能是"),
+                Plain("失败！你今天换老公次数太多了，你今天的老婆只能是"),
                 Plain(wife_now.split(".")[0]),
                 Image(assets / wife_now),
             ]
@@ -56,11 +55,11 @@ async def waifu(msg: Bot.MessageSession, change_: bool):
     )
 
 
-@wif.command("获取今日二次元老婆")
+@hsb.command("获取今日二次元老公")
 async def _(msg: Bot.MessageSession):
     await waifu(msg, False)
 
 
-@wif.command("change {换老婆}")
+@hsb.command("change {换老公}")
 async def _(msg: Bot.MessageSession):
     await waifu(msg, True)
--- modules/husband/database/models.py
+++ modules/husband/database/models.py
@@ -4,28 +4,28 @@ from tortoise import fields
 
 from core.database.base import DBModel
 
-table_prefix = "module_wife_"
+table_prefix = "module_husband_"
 
 
-class TodayWifeInfo(DBModel):
+class TodayHusbandInfo(DBModel):
     """
-    用户随机到的老婆
+    用户随机到的老公
 
     :param sender_id: 用户 ID。
-    :param wife_name: 随机的老婆名。
+    :param husband_name: 随机的老公名。
     :param timestamp: 时间戳。
     """
 
     sender_id = fields.CharField(max_length=512, pk=True)
-    wife_name = fields.CharField(max_length=512, null=True)
+    husband_name = fields.CharField(max_length=512, null=True)
     timestamp = fields.DatetimeField(auto_now=True)
 
     class Meta:
         table = f"{table_prefix}info"
 
     @classmethod
-    async def get_wife(cls, sender_id: str, name: str):
+    async def get_husband(cls, sender_id: str, name: str):
         info = (await cls.get_or_create(sender_id=sender_id))[0]
-        info.wife_name = name
+        info.husband_name = name
         await info.save()
         return True
